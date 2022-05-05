d755271d378a60245af1e99f3076857a4fb00e86
Shakir Ansari <shakir.ansari@live.com>
2022-05-05 12:34:47 +0530

    Update BinaryEncoderTest.java

--- jaxer-core/src/test/java/in/jaxer/core/encoders/BinaryEncoderTest.java
+++ jaxer-core/src/test/java/in/jaxer/core/encoders/BinaryEncoderTest.java
@@ -1,6 +1,9 @@
 
 package in.jaxer.core.encoders;
 
+import in.jaxer.core.exceptions.ValidationException;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -8,16 +11,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 /**
  *
  * @author Shakir
  */
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(Lifecycle.PER_CLASS)
 public class BinaryEncoderTest
 {
 
 	private Encoder encoder;
+
+	private final String encodedMessage = "211010003110010121101100311011002110111131000002111011131101111211100103110110021100100";
 
 	public BinaryEncoderTest()
 	{
@@ -45,10 +51,41 @@ public class BinaryEncoderTest
 	}
 
 	@Test
-	public void encodedMessageShouldNotBeNullWhenTextIsEmpty()
+	public void whenMessageIsEmpty()
 	{
-		System.out.println("encodedMessageShouldNotBeNullWhenTextIsEmpty");
-		String msg = "";
-		Assertions.assertNull(encoder.encode(msg));
+		System.out.println("whenMessageIsEmpty");
+		Assertions.assertThrows(ValidationException.class, () -> encoder.encode(""));
+		Assertions.assertThrows(ValidationException.class, () -> encoder.decode(""));
+	}
+
+	@Test
+	public void whenMessageIsNull()
+	{
+		System.out.println("whenMessageIsNull");
+		Assertions.assertThrows(ValidationException.class, () -> encoder.encode(null));
+		Assertions.assertThrows(ValidationException.class, () -> encoder.decode(null));
+	}
+
+	@Test
+	public void encodedMesssageShouldBeEqualsTo()
+	{
+		System.out.println("encodedMesssageShouldBeEqualsTo");
+
+		String response = encoder.encode("hello world");
+//		System.out.println("response: [" + response + "]");
+
+		MatcherAssert.assertThat(response, CoreMatchers.equalTo(encodedMessage));
+	}
+
+	@Test
+	public void decodedMesssageShouldBeEqualsTo()
+	{
+		System.out.println("encodedMesssageShouldBeEqualsTo");
+
+		String decodedMessage = "hello world";
+		String response = encoder.decode(encodedMessage);
+//		System.out.println("response: [" + response + "]");
+
+		MatcherAssert.assertThat(response, CoreMatchers.equalTo(decodedMessage));
 	}
 }
