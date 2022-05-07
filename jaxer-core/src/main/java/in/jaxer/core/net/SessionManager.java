be008320af00498a63b050b3bfa945aec32bd425
Shakir Ansari <shakir.ansari@live.com>
2022-05-07 23:44:44 +0530

    Update SessionManager.java

--- jaxer-core/src/main/java/in/jaxer/core/net/SessionManager.java
+++ jaxer-core/src/main/java/in/jaxer/core/net/SessionManager.java
@@ -3,16 +3,20 @@ package in.jaxer.core.net;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
 
 /**
  *
  * @author Shakir Ansari
  */
+@Log4j2
 public class SessionManager
 {
 
 	public static Object getObject(HttpServletRequest httpServletRequest, String key)
 	{
+		log.debug("key: {}", key);
+
 		HttpSession httpSession = httpServletRequest.getSession(false);
 		if (httpSession == null)
 		{
@@ -24,6 +28,8 @@ public class SessionManager
 
 	public static <T> T getObject(HttpServletRequest httpServletRequest, String key, Class<T> clazz)
 	{
+		log.debug("key: {}, objectType: {}", key, clazz);
+
 		HttpSession httpSession = httpServletRequest.getSession(false);
 		if (httpSession == null)
 		{
@@ -35,6 +41,8 @@ public class SessionManager
 
 	public static void setObject(HttpServletRequest httpServletRequest, String key, Object object)
 	{
+		log.debug("key: {}, object: {}", key, object);
+
 		if (object == null)
 		{
 			return;
@@ -43,6 +51,19 @@ public class SessionManager
 		HttpSession httpSession = httpServletRequest.getSession(true);
 
 		httpSession.setAttribute(key, object);
+	}
+
+	public static void removeObject(HttpServletRequest httpServletRequest, String key)
+	{
+		log.debug("key: {}", key);
+
+		HttpSession httpSession = httpServletRequest.getSession(false);
+		if (httpSession == null)
+		{
+			return;
+		}
+
+		httpServletRequest.removeAttribute(key);
 	}
 
 	public static void invalidate(HttpServletRequest httpServletRequest)
