c005e24552f56d04a51cfe45f51f78239637ef02
Shakir Ansari <shakir.ansari@live.com>
2022-05-21 00:25:43 +0530

    Update PropertyConfigurationManager.java

--- jaxer-core/src/main/java/in/jaxer/core/files/PropertyConfigurationManager.java
+++ jaxer-core/src/main/java/in/jaxer/core/files/PropertyConfigurationManager.java
@@ -1,4 +1,3 @@
-
 package in.jaxer.core.files;
 
 import in.jaxer.core.annotations.PropertyConfiguration;
@@ -6,13 +5,13 @@ import in.jaxer.core.annotations.PropertyKey;
 import in.jaxer.core.constants.Singletons;
 import in.jaxer.core.utilities.JValidator;
 import in.jaxer.core.utilities.PackageScanner;
+import lombok.extern.log4j.Log4j2;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Set;
-import lombok.extern.log4j.Log4j2;
 
 /**
- *
  * @author Shakir Ansari
  */
 @Log4j2
@@ -33,28 +32,28 @@ public class PropertyConfigurationManager
 		Set<Class> propertyConfigurationSet = PackageScanner.getClasses(basePackage, PropertyConfiguration.class);
 		log.debug("propertyConfigurationSet: {}", propertyConfigurationSet);
 
-		if (JValidator.isEmpty(propertyConfigurationSet))
+		if(JValidator.isEmpty(propertyConfigurationSet))
 		{
 			log.error("No class found with @{} in base package: {}", PropertyConfiguration.class.getName(), this.getBasePackage());
 			return;
 		}
 
 		String propertyFileName = null;
-		for (Class<?> propertyConfigurationClass : propertyConfigurationSet)
+		for(Class<?> propertyConfigurationClass : propertyConfigurationSet)
 		{
 			PropertyConfiguration propertyConfigurationAnnotation = propertyConfigurationClass.getAnnotation(PropertyConfiguration.class);
 			propertyFileName = propertyConfigurationAnnotation.value();
 			log.info("propertyFileName: {}", propertyFileName);
 
-			if (!propertyFileName.toLowerCase().endsWith(PROPERTIES_EXT))
+			if(!propertyFileName.toLowerCase().endsWith(PROPERTIES_EXT))
 			{
 				throw new IllegalArgumentException("Property file '" + propertyFileName + "' must be a valid '" + PROPERTIES_EXT + "' file");
 			}
 
-			try (JPropertyReader jPropertyReader = new JPropertyReader(propertyFileName))
+			try(JPropertyReader jPropertyReader = new JPropertyReader(propertyFileName))
 			{
 				Field[] fields = propertyConfigurationClass.getDeclaredFields();
-				if (fields == null || fields.length < 1)
+				if(fields == null || fields.length < 1)
 				{
 					log.warn("Field array found empty for class: {}", propertyConfigurationClass);
 					continue;
@@ -62,14 +61,13 @@ public class PropertyConfigurationManager
 
 				Object newInstance = propertyConfigurationClass.newInstance();
 
-				for (Field field : fields)
+				for(Field field : fields)
 				{
-					if (field.isAnnotationPresent(PropertyKey.class)
+					if(field.isAnnotationPresent(PropertyKey.class)
 							//							&& Modifier.isStatic(field.getModifiers())
 							&& !Modifier.isFinal(field.getModifiers()))
 					{
-						if (Modifier.isPrivate(field.getModifiers())
-								|| Modifier.isProtected(field.getModifiers()))
+						if(Modifier.isPrivate(field.getModifiers()) || Modifier.isProtected(field.getModifiers()))
 						{
 							field.setAccessible(true);
 						}
@@ -79,31 +77,30 @@ public class PropertyConfigurationManager
 
 						log.debug("[key={}]=[value={}]", appPropertyKey.name(), value);
 
-						if (value != null)
+						if(value != null)
 						{
-							if (appPropertyKey.type().getName().equals(String.class.getName())
-									|| appPropertyKey.type().getName().equals(Character.class.getName()))
+							if(appPropertyKey.type().getName().equals(String.class.getName()) || appPropertyKey.type().getName().equals(Character.class.getName()))
 							{
 								field.set(newInstance, value);
-							} else if (appPropertyKey.type().getName().equals(Integer.class.getName()))
+							} else if(appPropertyKey.type().getName().equals(Integer.class.getName()))
 							{
 								field.set(newInstance, Integer.valueOf(value));
-							} else if (appPropertyKey.type().getName().equals(Long.class.getName()))
+							} else if(appPropertyKey.type().getName().equals(Long.class.getName()))
 							{
 								field.set(newInstance, Long.valueOf(value));
-							} else if (appPropertyKey.type().getName().equals(Float.class.getName()))
+							} else if(appPropertyKey.type().getName().equals(Float.class.getName()))
 							{
 								field.set(newInstance, Float.valueOf(value));
-							} else if (appPropertyKey.type().getName().equals(Double.class.getName()))
+							} else if(appPropertyKey.type().getName().equals(Double.class.getName()))
 							{
 								field.set(newInstance, Double.valueOf(value));
-							} else if (appPropertyKey.type().getName().equals(Short.class.getName()))
+							} else if(appPropertyKey.type().getName().equals(Short.class.getName()))
 							{
 								field.set(newInstance, Short.valueOf(value));
-							} else if (appPropertyKey.type().getName().equals(Byte.class.getName()))
+							} else if(appPropertyKey.type().getName().equals(Byte.class.getName()))
 							{
 								field.set(newInstance, Byte.valueOf(value));
-							} else if (appPropertyKey.type().getName().equals(Boolean.class.getName()))
+							} else if(appPropertyKey.type().getName().equals(Boolean.class.getName()))
 							{
 								field.set(newInstance, Boolean.valueOf(value));
 							} else
@@ -112,14 +109,14 @@ public class PropertyConfigurationManager
 								try
 								{
 									field.set(newInstance, Singletons.getGson().fromJson(value, appPropertyKey.type()));
-								} catch (Exception exception)
+								} catch(Exception exception)
 								{
 									log.error("Exception", exception);
 
 									try
 									{
 										field.set(newInstance, value);
-									} catch (Exception innerException)
+									} catch(Exception innerException)
 									{
 										log.error("innerException", innerException);
 									}
@@ -127,8 +124,7 @@ public class PropertyConfigurationManager
 							}
 						}
 
-						if (Modifier.isPrivate(field.getModifiers())
-								|| Modifier.isProtected(field.getModifiers()))
+						if(Modifier.isPrivate(field.getModifiers()) || Modifier.isProtected(field.getModifiers()))
 						{
 							field.setAccessible(false);
 						}
