20e8635d74cfba9df2dc2ed8c2a96219b3a82dd8
Shakir Ansari <shakir.ansari@live.com>
2022-05-23 18:09:36 +0530

    Update HashHandler.java

--- jaxer-core/src/main/java/in/jaxer/core/utilities/HashHandler.java
+++ jaxer-core/src/main/java/in/jaxer/core/utilities/HashHandler.java
@@ -1,23 +1,24 @@
-
 package in.jaxer.core.utilities;
 
 import in.jaxer.core.constants.Constants;
+import in.jaxer.core.exceptions.JaxerCoreException;
+import lombok.extern.log4j.Log4j2;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.Formatter;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 
 /**
- *
  * @author Shakir Ansari
  */
+@Log4j2
 public class HashHandler
 {
-
 	private static String toHexString(MessageDigest digest)
 	{
 		return new BigInteger(1, digest.digest()).toString(16);
@@ -35,6 +36,7 @@ public class HashHandler
 
 	private static String getHash(String msg, String encodeType)
 	{
+		log.debug("msg: {}, encodeType: {}", msg, encodeType);
 		try
 		{
 			MessageDigest digest = MessageDigest.getInstance(encodeType);
@@ -45,12 +47,14 @@ public class HashHandler
 			return toHexString(digest);
 		} catch (Exception ex)
 		{
-			throw new RuntimeException("Exception occured while generating hash", ex);
+			log.error("Exception", ex);
+			throw new JaxerCoreException("Exception occured while generating hash", ex);
 		}
 	}
 
 	private static String getHash(String msg, String key, String hashType)
 	{
+		log.debug("msg: {}, key: {}, hashType", msg, key, hashType);
 		try
 		{
 			SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), hashType);
@@ -59,12 +63,14 @@ public class HashHandler
 			return toHexString(mac.doFinal(msg.getBytes()));
 		} catch (Exception exception)
 		{
-			throw new RuntimeException("Exception occured while generating hash", exception);
+			log.error("Exception", exception);
+			throw new JaxerCoreException("Exception occured while generating hash", exception);
 		}
 	}
 
 	private static String getFileChecksum(String encodeType, InputStream inputStream)
 	{
+		log.debug("encodeType: {}, inputStream: {}", encodeType, inputStream);
 		try
 		{
 			MessageDigest digest = MessageDigest.getInstance(encodeType);
@@ -80,18 +86,22 @@ public class HashHandler
 
 		} catch (Exception exception)
 		{
-			throw new RuntimeException("Exception occured while generating file hash", exception);
+			log.error("Exception", exception);
+			throw new JaxerCoreException("Exception occured while generating file hash", exception);
 		}
 	}
 
 	private static String getFileChecksum(String encodeType, File file)
 	{
+		log.debug("encodeType: {}, file: {}", encodeType, file);
+
 		try (FileInputStream fileInputStream = new FileInputStream(file))
 		{
 			return getFileChecksum(encodeType, fileInputStream);
 		} catch (Exception ex)
 		{
-			throw new RuntimeException("Exception occured while generating file hash", ex);
+			log.error("Exception", ex);
+			throw new JaxerCoreException("Exception occured while generating file hash", ex);
 		}
 	}
 
