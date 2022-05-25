45a70c6db9bd9e753e3257249e254905fc182710
Shakir Ansari <shakir.ansari@live.com>
2022-05-25 22:24:22 +0530

    Create TimeDifference.java

---
+++ jaxer-core/src/main/java/in/jaxer/core/dtos/TimeDifference.java
@@ -0,0 +1,26 @@
+package in.jaxer.core.dtos;
+
+/**
+ * @author Shakir
+ * date 2022-05-25 19:44
+ */
+public class TimeDifference
+{
+	public long seconds = 0;
+	public long minutes = 0;
+	public long hours = 0;
+	public long days = 0;
+
+	@Override
+	public String toString()
+	{
+		String response = "TimeDifference: ";
+
+		response += days != 0 ? days + " days, " : "";
+		response += hours != 0 ? hours + " hours, " : "";
+		response += minutes != 0 ? minutes + " minutes, " : "";
+		response += seconds != 0 ? seconds + " seconds " : "";
+
+		return response;
+	}
+}
