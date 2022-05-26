c0f66728b946ef0a14f118d119f3cbfe809f8d69
Shakir Ansari <shakir.ansari@live.com>
2022-05-26 15:53:21 +0530

    Update ClientInstructionDto.java

--- jaxer-api/src/main/java/in/jaxer/api/dtos/ClientInstructionDto.java
+++ jaxer-api/src/main/java/in/jaxer/api/dtos/ClientInstructionDto.java
@@ -1,11 +1,27 @@
 package in.jaxer.api.dtos;
 
+import lombok.Getter;
 import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Shakir Ansari
  */
 @ToString
-public class ClientInstructionDto extends InstructionsDto
+public class ClientInstructionDto
 {
+	@Getter
+	private List<String> instructionList;
+
+	public void addInstruction(String instruction)
+	{
+		if (this.instructionList == null)
+		{
+			this.instructionList = new ArrayList<>();
+		}
+
+		this.instructionList.add(instruction);
+	}
 }
