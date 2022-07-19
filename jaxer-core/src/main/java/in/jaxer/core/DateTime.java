49b9ea776e224af8e056b41f76ce55d6298a26b0
Shakir Ansari <shakir.ansari@live.com>
2022-07-19 15:43:33 +0530

    added @since

--- jaxer-core/src/main/java/in/jaxer/core/DateTime.java
+++ jaxer-core/src/main/java/in/jaxer/core/DateTime.java
@@ -9,6 +9,7 @@ import java.util.Date;
 /**
  * @author Shakir
  * @date 20-06-2022
+ * @since 1.0.9-beta
  */
 public class DateTime
 {
@@ -32,6 +33,9 @@ public class DateTime
 		this.date = new Date(millseconds);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addDateTime(int field, int amount)
 	{
 		Calendar cal = Calendar.getInstance();
@@ -40,47 +44,74 @@ public class DateTime
 		return new DateTime(cal.getTime());
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addMilliSeconds(Date date, int milliSeconds)
 	{
 		return addDateTime(Calendar.MILLISECOND, milliSeconds);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addSeconds(Date date, int seconds)
 	{
 		return addDateTime(Calendar.SECOND, seconds);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addMinutes(Date date, int minutes)
 	{
 		return addDateTime(Calendar.MINUTE, minutes);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addHours(Date date, int hours)
 	{
 		return addDateTime(Calendar.HOUR, hours);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addDays(Date date, int days)
 	{
 		return addDateTime(Calendar.DATE, days);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addMonths(Date date, int months)
 	{
 		return addDateTime(Calendar.MONTH, months);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public DateTime addYears(Date date, int years)
 	{
 		return addDateTime(Calendar.YEAR, years);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	@Override
 	public String toString()
 	{
 		return new SimpleDateFormat(DEFAULT_DATE_TIME_FORMAT).format(date);
 	}
 
+	/**
+	 * @since 1.0.9-beta
+	 */
 	public String toString(String pattern)
 	{
 		return new SimpleDateFormat(pattern).format(date);
