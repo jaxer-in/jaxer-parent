4a266d4fd6c86c1b98cf9fb22e1d03e982eaf608
Shakir Ansari <shakir.ansari@live.com>
2022-08-08 18:17:01 +0530

    added DateTimeUtils

--- jaxer-core/src/main/java/in/jaxer/core/DateTimeUtils.java
+++ jaxer-core/src/main/java/in/jaxer/core/DateTimeUtils.java
@@ -7,28 +7,55 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
+ * ** ************************************
+ * ** Useful Keys
+ * ** ************************************
+ * G Era designator :AD
+ * y Year :1996; 96
+ * Y Week year :2009; 09
+ * M Month in year :July; Jul; 07
+ * w Week in year :27
+ * W Week in month :2
+ * D Day in year :189
+ * d Day in month :10
+ * F Day of week in month :2
+ * E Day name in week :Tuesday; Tue
+ * u Day number of week :(1 = Monday, …, 7 = Sunday)	1
+ * a Am/pm marker	PM
+ * H Hour in day (0-23)	0
+ * k Hour in day (1-24)	24
+ * K Hour in am/pm (0-11)	0
+ * h Hour in am/pm (1-12)	12
+ * m Minute in hour	30
+ * s Second in minute	55
+ * S Millisecond	978
+ * z Time zone	Pacific Standard Time; PST; GMT-08:00
+ * Z Time zone	-0800
+ * X Time zone	-08; -0800; -08:00
+ * ** *************************************
+ *
  * @author Shakir
  * @date 20-06-2022
  * @since 1.0.9-beta
  */
-public class DateTime
+public class DateTimeUtils
 {
 	public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
 	@Getter
 	private final Date date;
 
-	public DateTime()
+	public DateTimeUtils()
 	{
 		this.date = new Date();
 	}
 
-	public DateTime(Date date)
+	public DateTimeUtils(Date date)
 	{
 		this.date = date;
 	}
 
-	public DateTime(long millseconds)
+	public DateTimeUtils(long millseconds)
 	{
 		this.date = new Date(millseconds);
 	}
@@ -36,18 +63,18 @@ public class DateTime
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addDateTime(int field, int amount)
+	public DateTimeUtils addDateTime(int field, int amount)
 	{
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(this.date);
 		cal.add(field, amount); //minus number would decrement
-		return new DateTime(cal.getTime());
+		return new DateTimeUtils(cal.getTime());
 	}
 
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addMilliSeconds(Date date, int milliSeconds)
+	public DateTimeUtils addMilliSeconds(Date date, int milliSeconds)
 	{
 		return addDateTime(Calendar.MILLISECOND, milliSeconds);
 	}
@@ -55,7 +82,7 @@ public class DateTime
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addSeconds(Date date, int seconds)
+	public DateTimeUtils addSeconds(Date date, int seconds)
 	{
 		return addDateTime(Calendar.SECOND, seconds);
 	}
@@ -63,7 +90,7 @@ public class DateTime
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addMinutes(Date date, int minutes)
+	public DateTimeUtils addMinutes(Date date, int minutes)
 	{
 		return addDateTime(Calendar.MINUTE, minutes);
 	}
@@ -71,7 +98,7 @@ public class DateTime
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addHours(Date date, int hours)
+	public DateTimeUtils addHours(Date date, int hours)
 	{
 		return addDateTime(Calendar.HOUR, hours);
 	}
@@ -79,7 +106,7 @@ public class DateTime
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addDays(Date date, int days)
+	public DateTimeUtils addDays(Date date, int days)
 	{
 		return addDateTime(Calendar.DATE, days);
 	}
@@ -87,7 +114,7 @@ public class DateTime
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addMonths(Date date, int months)
+	public DateTimeUtils addMonths(Date date, int months)
 	{
 		return addDateTime(Calendar.MONTH, months);
 	}
@@ -95,7 +122,7 @@ public class DateTime
 	/**
 	 * @since 1.0.9-beta
 	 */
-	public DateTime addYears(Date date, int years)
+	public DateTimeUtils addYears(Date date, int years)
 	{
 		return addDateTime(Calendar.YEAR, years);
 	}
