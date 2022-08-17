0d02b2e7d8a4cb1b64686c0d635ef45553d9fe94
Shakir Ansari <shakir.ansari@live.com>
2022-08-17 17:06:26 +0530

    Create DatabaseUtils.java

---
+++ jaxer-core/src/main/java/in/jaxer/core/DatabaseUtils.java
@@ -0,0 +1,44 @@
+package in.jaxer.core;
+
+/**
+ * @author Shakir
+ * @date 17-08-2022
+ * @since v1.1.0-beta
+ */
+public class DatabaseUtils
+{
+	/**
+	 * For MySQL database
+	 *
+	 * @since v1.1.0-beta
+	 */
+	public static final String COM_MYSQL_JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+	/**
+	 * For H2 database
+	 *
+	 * @since v1.1.0-beta
+	 */
+	public static final String ORG_H2_DRIVER = "org.h2.Driver";
+
+	/**
+	 * For HSQL database
+	 *
+	 * @since v1.1.0-beta
+	 */
+	public static final String JDBC_JDBCDRIVER = "jdbc.jdbcDriver";
+
+	/**
+	 * For Oracle database
+	 *
+	 * @since v1.1.0-beta
+	 */
+	public static final String ORACLE_JDBC_DRIVER_ORACLEDRIVER = "oracle.jdbc.driver.OracleDriver";
+
+	/**
+	 * For sqlite database
+	 *
+	 * @since v1.1.0-beta
+	 */
+	public static final String ORG_SQLITE_JDBC = "org.sqlite.JDBC";
+}
